530ae2cd204cfcb3f83d70b8d5d6b83c681e07f8
rinku-kr <rk24736599@gmail.com>
2023-12-16 23:46:11 +0530

    Using grid view

--- lib/categories.dart
+++ lib/categories.dart
@@ -7,7 +7,40 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Pick your category!'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: const [
+          Text(
+            'data 1',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data 2',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data 3',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data 4',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data 5',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            'data 6',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
