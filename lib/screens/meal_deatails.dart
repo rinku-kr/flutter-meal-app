39023ebc40925db821a8acbb26fef594ddd310fc
rinku-kr <rk24736599@gmail.com>
2023-12-29 13:05:52 +0530

    Improving mealDetail screen

--- lib/screens/meal_deatails.dart
+++ lib/screens/meal_deatails.dart
@@ -10,11 +10,58 @@ class MealDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(meal.title),
-      ),
-      body: Image.network(meal.imageUrl,
-          height: 200, width: double.infinity, fit: BoxFit.cover),
-    );
+        appBar: AppBar(
+          title: Text(meal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.network(meal.imageUrl,
+                  height: 200, width: double.infinity, fit: BoxFit.cover),
+              Text(
+                'Ingredient',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              for (final ingredient in meal.ingredients)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 4),
+                  child: Text(
+                    ingredient,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              const SizedBox(
+                height: 14,
+              ),
+              Text(
+                'Ingredient',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold),
+              ),
+              for (final step in meal.steps)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 4, horizontal: 4),
+                  child: Text(
+                    step,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+            ],
+          ),
+        ));
   }
 }
