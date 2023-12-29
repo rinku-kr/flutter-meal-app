3922227e27d8a6725a3e811b1ab05670640ea0e5
rinku-kr <rk24736599@gmail.com>
2023-12-29 11:45:49 +0530

    Improving meal item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
   const MealItem({
@@ -10,6 +11,16 @@ class MealItem extends StatelessWidget {
   });
 
   final Meal meal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() +
+        meal.complexity.name.substring(1);
+  }
+
+  String get affordabilityText {
+    return meal.affordability.name[0].toUpperCase() +
+        meal.affordability.name.substring(1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +65,27 @@ class MealItem extends StatelessWidget {
                     height: 12,
                   ),
                   Row(
-                    children: [],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MealItemTrait(
+                        icon: Icons.schedule,
+                        label: '${meal.duration} min',
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.work,
+                        label: complexityText,
+                      ),
+                      const SizedBox(
+                        width: 12,
+                      ),
+                      MealItemTrait(
+                        icon: Icons.attach_money,
+                        label: affordabilityText,
+                      ),
+                    ],
                   )
                 ]),
               ))
